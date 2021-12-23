e1900a4567a4a99aafdfd381641f151661d13f26
nakashimmer <nakacma@gmail.com>
2021-12-23 09:47:46 +0900

    up

--- ga.js
+++ ga.js
@@ -1,6 +1,6 @@
 //google analytics
 document.write(
-	'<script async src="https://www.googletagmanager.com/gtag/js?id=UA-165695378-1"></script>'
+	'<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136606141-1"></script>'
 );
 
 window.dataLayer = window.dataLayer || [];
@@ -9,4 +9,4 @@ function gtag() {
 	dataLayer.push(arguments);
 }
 
-gtag('js', new Date()); gtag('config', 'UA-165695378-1');
+gtag('js', new Date()); gtag('config', 'UA-136606141-1');
